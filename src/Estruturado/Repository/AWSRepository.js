b4ab6da080955cf8bb4847d9793bf90bee23ff54
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-02-18 15:58:01 -0300

    Create AWSRepository.js

---
+++ src/Estruturado/Repository/AWSRepository.js
@@ -0,0 +1,27 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+    region: 'us-east-1',
+    accessKeyId: 'AKIA5RRHCKYZU46MAWLR',
+    secretAccessKey: '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'
+});
+
+const s3 = new AWS.S3();
+
+class AWSRepository {
+    async buscarImagem(referencia) {
+        try {
+            const params = {
+                Bucket: 'bucketmi74',
+                Key: referencia
+            };
+
+            const url = s3.getSignedUrl('getObject', params);
+            return { url };
+        } catch (error) {
+            throw new Error("Erro ao buscar imagem no S3: " + error.message);
+        }
+    }
+}
+
+module.exports = new AWSRepository();
