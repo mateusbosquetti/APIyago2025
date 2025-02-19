e9339aea297bab3061dcca77b9f70e83dcfc3431
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-02-18 21:38:12 -0300

    AWSService-Documentação-Testada
    A classe AWSService tem todas as regraas de negócio relacionadas a AWS. Estão todos os métodos devidamente comentados e testados pelo aluno Mateus Henrique Bosquetti.

--- src/Estruturado/Service/AWSService.js
+++ src/Estruturado/Service/AWSService.js
@@ -2,15 +2,36 @@ const AWSRepository = require('../Repository/AWSRepository');
 const path = require('path');
 const fs = require('fs');
 
+/**
+ * @author Mateus Henrique Bosquetti
+ * @version 1.0
+ * @since 18/02/2025
+ */
 class AWSService {
+
+    /**
+     * Esse método chama o método da AWSRepository de buscarImagem
+     * @param { referencia } referencia 
+     * @returns resposta do método buscarImagem da AWSRepository
+     */
     async buscarImagem(referencia) {
         return await AWSRepository.buscarImagem(referencia);
     }
 
+    /**
+     * Esse método chama o método da AWSRepository de uploadImagem
+     * @param { file } file 
+     * @returns resposta do método uploadImagem da AWSRepository
+     */
     async uploadImagem(file) {
         return await AWSRepository.uploadImagem(file);
     }
 
+    /**
+     * Esse método pega o arquivo e o caminho do arquivo ("/Downloads") e baixa a imagem la
+     * @param { refencia } referencia 
+     * @returns O caminho da imagem que foi baixada
+     */
     async downloadImagem(referencia) {
         const fileData = await AWSRepository.downloadImagem(referencia);
 
