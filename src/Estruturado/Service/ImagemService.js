9487f3a7aab82fd513ae81d0820f6305fd2b91c5
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-03-04 16:05:27 -0300

    feature/ImagemRepository deletando códigos comentados

--- src/Estruturado/Service/ImagemService.js
+++ src/Estruturado/Service/ImagemService.js
@@ -22,37 +22,4 @@ class ImagemService {
 
 }
 
-
-    /*
-buscarImagemPeloID(request, response) {
-        const id = request.params
-        database.select("*").table("imagem").where({id:id.id}).then(data => {
-            response.json(data);
-        }).catch(error => {
-            response.send(400);
-        })
-    }
-
-    atualizarImagem(request, response){
-        const id = request.params;
-        const {referencia, titulo} = request.body;
-        
-        database.where({id:id.id}).update({referencia:referencia, titulo:titulo}).table("imagem").then(data=>{
-            response.json({message:"Imagem atualizado com sucesso"})
-        }).catch(error=>{
-            response.json(error);
-        })
-    }
-    
-    removerImagem(request, response) {
-        const id = request.params;
-
-        database.where({id:id.id}).del().table("imagem").then(data=>{
-            response.json({message:"Imagem deletada com sucesso"})
-        }).catch(error=>{
-            response.json(error);
-        })
-    }
-    */
-
 module.exports = new ImagemService();
